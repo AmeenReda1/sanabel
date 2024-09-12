547b4a188d688ccd9373f8a8005afd294ccb1b20
AmeenReda1 <ameenreda1@gmail.com>
2024-09-12 12:44:42 +0300

    add role guard in delete scpecific route

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
+import { RoleGuard } from 'src/common/gurads/permission.guard';
+import { JwtAuthGuard } from 'src/user/guards/user-jwt.guard';
+import { UserRole } from 'src/user/user.entity';
+import { Role } from 'src/common/decorators/role.decorator';
 
 @Controller('customer')
 export class CustomerController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(private readonly customerService: CustomerService) { }
 
   @Post()
   create(@Body() createCustomerDto: CreateCustomerDto) {
@@ -15,6 +19,8 @@ export class CustomerController {
   findAll() {
     return this.customerService.findAll();
   }
+  @Role(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.customerService.remove(+id);
